adc39977a30690ab80dd4ded3b9ff01ddd30e3fc
Cermicto <123908856+Cermicto@users.noreply.github.com>
2023-01-29 18:23:41 -0600

    d w n loop si st base set

--- Initr.js
+++ Initr.js
@@ -1,4 +1,18 @@
-d=document
-d.gi = d.getElementById
+d = document
+w = window
+w.n = w.navigator
+d.gebi = d.getElementById
+d.gebcn = d.getElementByClassName
+d.gebtn = d.getElementByTagName
+t = this
+th = t
 
 si = setInterval
+st = setTimeout
+
+l = 0
+lc = false
+lm = 0
+lmc = false
+lmn = 0
+lmnc = false
